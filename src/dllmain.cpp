c0ce41c803b772c1a69f912b7e68a96a047b2897
星梦 <starsdream00@icloud.com>
2023-07-10 14:33:25 +0800

    修复

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -3,43 +3,47 @@
 #pragma comment(lib, "../SDK/lib/SymDBHelper.lib")
 #pragma comment(lib, "../SDK/lib/LiteLoader.lib")
 
+#include <llapi/mc/ActorUniqueID.hpp>
+#include <llapi/mc/Level.hpp>
 #include <llapi/mc/Player.hpp>
 #include <llapi/mc/Container.hpp>
 #include <llapi/mc/ItemStack.hpp>
+#include <llapi/GlobalServiceAPI.h>
+#include <llapi/ScheduleAPI.h>
 
 void reloadItem(Player& player, int id, int auxValue) {
-    if (!player.getSelectedItem().isNull()) {
-        return;
-    }
-    int slot = player.getSelectedItemSlot();
-    Container& inventory = player.getInventory();
-    for (int i = 0; i < inventory.getContainerSize(); ++i) {
-        if (i == slot) {
-            continue;
-        }
-        ItemStack const& itemStack = inventory.getItem(i);
-        if (!itemStack.sameItem(id, auxValue)) {
-            continue;
-        }
-        if (i < 9) {
-            player.setSelectedSlot(i);
+    ActorUniqueID const& uniqueID = player.getUniqueID();
+    Schedule::nextTick([uniqueID, id, auxValue]() {
+        Player* player = Global<Level>->getPlayer(uniqueID);
+        if (!player || !player->getSelectedItem().isNull()) {
             return;
         }
-        ItemStack newItemStack = itemStack.clone();
-        inventory.setItem(i, ItemStack::EMPTY_ITEM);
-        player.setSelectedItem(newItemStack);
-        player.sendInventory(true);
-        return;
-    }
+        int slot = player->getSelectedItemSlot();
+        Container& inventory = player->getInventory();
+        for (int i = 0; i < inventory.getContainerSize(); ++i) {
+            if (i == slot) {
+                continue;
+            }
+            ItemStack const& itemStack = inventory.getItem(i);
+            if (!itemStack.sameItem(id, auxValue)) {
+                continue;
+            }
+            if (i < 9) {
+                player->setSelectedSlot(i);
+                return;
+            }
+            ItemStack newItemStack = itemStack.clone();
+            inventory.setItem(i, ItemStack::EMPTY_ITEM);
+            player->setSelectedItem(newItemStack);
+            player->sendInventory(true);
+            return;
+        }
+    });
 }
 
 #include <llapi/mc/ItemStackBase.hpp>
 #include <llapi/mc/Actor.hpp>
-#include <llapi/mc/ActorUniqueID.hpp>
-#include <llapi/mc/Level.hpp>
 #include <llapi/HookAPI.h>
-#include <llapi/ScheduleAPI.h>
-#include <llapi/GlobalServiceAPI.h>
 
 TInstanceHook(void, "?useItem@Player@@UEAAXAEAVItemStackBase@@W4ItemUseMethod@@_N@Z", Player, ItemStackBase& a2, ItemUseMethod a3, bool a4) {
     int id = a2.getId();
@@ -48,14 +52,7 @@ TInstanceHook(void, "?useItem@Player@@UEAAXAEAVItemStackBase@@W4ItemUseMethod@@_
     if (!a2.isNull()) {
         return;
     }
-    ActorUniqueID const& uniqueID = getUniqueID();
-    Schedule::nextTick([uniqueID, id, auxValue]() {
-        Player* player = Global<Level>->getPlayer(uniqueID);
-        if (!player) {
-            return;
-}
-        reloadItem(*player, id, auxValue);
-    });
+    reloadItem(*this, id, auxValue);
 }
 TInstanceHook(bool, "?hurtAndBreak@ItemStackBase@@QEAA_NHPEAVActor@@@Z", ItemStackBase, int a2, Actor& a3) {
     int id = getId();
@@ -64,13 +61,6 @@ TInstanceHook(bool, "?hurtAndBreak@ItemStackBase@@QEAA_NHPEAVActor@@@Z", ItemSta
     if (!isNull() || !a3.isPlayer()) {
         return result;
     }
-    ActorUniqueID const& uniqueID = a3.getUniqueID();
-    Schedule::nextTick([uniqueID, id, auxValue]() {
-        Player* player = Global<Level>->getPlayer(uniqueID);
-        if (!player) {
-            return;
-        }
-        reloadItem(*player, id, auxValue);
-    });
+    reloadItem((Player&)a3, id, auxValue);
     return result;
 }
